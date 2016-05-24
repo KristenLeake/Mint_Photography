70deb6c996bd354413b3f7bbb47fd4530b0206c3
Kristen Leake <kristenleake@gmail.com>
2016-05-24 11:25:03 -0400

    reorganized and linked nav bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
+//     $(document).ready(function() {
+//         $(".toggle-nav").click(function(){
+//             $(".green").toggleClass("hidden");
+//         });
+// });
 
 
-    $(document).ready(function() {
-        $(".toggle-nav").click(function(){
-            $(".green").toggleClass("hidden");
-        });
-});
+// Bootstrap Accordian JS //
+$('#collapseOne').collapse({
+    toggle: false
+})
+
+$('#collapseTwo').collapse({
+    toggle: false
+})
+
+$('#collapseThree').collapse({
+    toggle: false
+})
+
+// $('#collapsibleOne').on('hidden.bs.collapse', function () {
+//   // do something…
+// })
